af1c9aa1d0ac3c7dbcb9bdd4fb761140b390b216
Robert <klegsy@gmail.com>
2015-11-09 15:50:18 +0000

    Changed name to 2 Years and Counting

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default = timezone.now)
-    published_date = models.DateTimeField(blank=-True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -28,3 +28,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+
+
+
